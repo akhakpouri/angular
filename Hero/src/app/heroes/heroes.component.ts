13bbb237a192145f12635b42307ca0e0fbe4518c
Ali Khakpouri <khakpouria@tessco.com>
2018-06-17 12:55:46 -0400

    Heroes and messages are done.

--- Hero/src/app/heroes/heroes.component.ts
+++ Hero/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
@@ -9,14 +10,17 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  onSelect(hero: Hero): void{
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  constructor() { }
-
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
-
+  ngOnInit() {
+    this.getHeroes();
+  }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 }
