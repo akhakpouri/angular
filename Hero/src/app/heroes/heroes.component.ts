d6aad0ac027b6b47e4688ebb5d689d0909944b4e
Ali Khakpouri <khakpouria@tessco.com>
2018-06-17 21:13:04 -0400

    Added the dashboard component. Moved the hero component into a routing component

--- Hero/src/app/heroes/heroes.component.ts
+++ Hero/src/app/heroes/heroes.component.ts
@@ -10,11 +10,7 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  heroes: Hero[];  
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
